b96ed10d0fde6545fc7b952fb2f93128834e7faf
Simon Julier <s.julier@ucl.ac.uk>
2022-02-16 08:26:09 +0000

    Added solutions.

---
+++ Lab_Week_05_-_Value_Functions,_Policies_and_Policy_Iteration/Solutions/recycling_robot/policy_evaluator.py
@@ -0,0 +1,99 @@
+'''
+Created on 29 Jan 2022
+
+@author: ucacsjj
+'''
+
+import copy
+
+import numpy as np
+
+# This class implements the policy evaluation algorithm.
+
+from .robot_states_and_actions import *
+
+class PolicyEvaluator:
+
+    def __init__(self, environment):
+        
+        # The environment the system works with        
+        self._environment = environment
+
+        # The discount factor        
+        self._gamma = 1
+        
+        # Threshold on maximum change in the value function to test
+        # for convergence in policy evaluation
+        self._theta = 1e-3
+        
+        # Flag to show if initialized
+        self._initialized = False
+        
+        # Working scratch variables for the current value function
+        self._v= None
+
+        # The maximum number of times the policy evaluation algorithm
+        # will be run before the for loop is exited.
+        self._max_policy_evaluation_steps = 5000
+
+    def initialize(self, pi, initial_v):
+        self._pi = copy.deepcopy(pi)
+        self._v = copy.deepcopy(initial_v)
+        print(f'initial_v={initial_v}')
+
+    def evaluate_policy(self):
+
+        # Get the environment
+        environment = self._environment
+
+        # Execute the loop at least once
+        
+        for iteration in range(self._max_policy_evaluation_steps):
+            
+            delta = 0
+
+            for s in range(RobotBatteryState.NUMBER_OF_STATES):
+               
+                # Special case: discharged always goes to the terminal state, so skip it
+                if s == RobotBatteryState.DISCHARGED:
+                    continue
+                
+                # Get the action distribution
+                pia = self._pi.get(s)
+
+                # Store the old value of the state value function to test for
+                # convergence
+                old_v = self._v[s]
+                
+                new_v = 0
+                
+                # Iterate over all available actions
+                for a in range(RobotActions.NUMBER_OF_ACTIONS):
+                    
+                    # Discard zero probability actions
+                    if pia[a] == 0:
+                        continue
+
+                    # Compute p(s',r|s,a)
+                    s_prime, r, p = environment.next_state_and_reward_distribution(s, a)
+ 
+                    for t in range(len(p)):
+                        if p[t] > 0:
+                            next_contribution = pia[a] * p[t] * (r  + self._gamma * self._v[s_prime[t]])
+                            new_v += next_contribution
+                    
+                # Set the new value in the value function
+                self._v[s] = new_v
+                                    
+                # Update the maximum deviation
+                delta = max(delta, abs(old_v-new_v))
+            
+            # Terminate the loop if either the change was very small
+            if delta < self._theta:
+                    break
+
+        print(f'Terminated after {iteration} iterations')
+        print(f'converged_v={self._v}')
+        
+        return self._v
+     
